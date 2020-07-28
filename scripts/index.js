e23591b146a22c2ab044d76f452eb949e6a83266
Sennikova Alexandra <SennikovaAlexandra01@yandex.ru>
2020-07-28 20:47:52 +0300

    lesson01

---
+++ scripts/index.js
@@ -0,0 +1,31 @@
+import { radioPlayerInit } from "./radioPlayer.js";
+import { musicPlayerInit } from "./musicPlayer.js"
+import { videoPlayerInit } from "./videoPlayer.js"
+
+
+const playerBtn = document.querySelectorAll('.player-btn');
+const playerBlock = document.querySelectorAll('.player-block');
+const temp = document.querySelector('.temp');
+
+const deativationPlayer = () => {
+    temp.style.display = 'none';
+    playerBtn.forEach(item => item.classList.remove('active'));
+    playerBlock.forEach(item => item.classList.remove('active'));
+}
+
+
+
+playerBtn.forEach((btn, index) => btn.addEventListener('click', () => {
+        deativationPlayer();
+        btn.classList.add('active');
+        playerBlock[index].classList.add('active');
+
+    }));
+
+
+
+
+
+radioPlayerInit();
+musicPlayerInit();
+videoPlayerInit();
